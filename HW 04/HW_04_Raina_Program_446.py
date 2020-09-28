4d94eaf91c045dd438af32f3b4d549c2807c478d
Navid Nafiuzzaman <mxn4459@rit.edu>
2020-09-27 23:01:44 -0400

    Raina Program Updated

--- HW 04/HW_04_Raina_Program_446.py
+++ HW 04/HW_04_Raina_Program_446.py
@@ -1,3 +1,9 @@
+"""
+Name: Nikhil Raina <nxr5013@rit.edu>
+FileName: HW_04_Raina_Program_446.py
+Case # 446
+"""
+
 import BirdBathFunction_446_v420 as bbf
 import numpy as np
 
@@ -6,18 +12,19 @@ if __name__ == "__main__":
     best_fractional_volume  = bbf.BirdbathFunc446( -7.1250, 2.0000, 5.4668 )
 
     # The minimum and maximum range values for each parameter for the bird function
-    min_params = [-45.0, -45.0, -45.0]
+    min_params = [-30.0, -30.0, -30.0]
     max_params = [45.0, 45.0, 45.0]
 
     # The best values for the range values.
     # This will be changing each loop iteration in order to get the best values 
     # This will be done by reducing the range for search every loop
-    best_params = [-12.53911577994586, 8.036903507163515, 47.87125362243387]
+    # best_params = [-12.53911577994586, 8.036903507163515, 47.87125362243387]
+    best_params = [-0.2, 0.2, 0.2]
     # Initial degree increment value during each iteration
     degree_increment = 10
 
     # value to ensure that the loop doesn't go on for too long or plays with small values
-    while degree_increment > 0.0025:
+    while degree_increment > 0.5:
         rotate_min = best_params[0] - (degree_increment * range_extender)
         rotate_max = best_params[0] + (degree_increment * range_extender)
         tilt_min = best_params[1] - (degree_increment * range_extender)
@@ -60,8 +67,8 @@ if __name__ == "__main__":
                 # print('ROTATE:', best_params[0], '\nTILT:', best_params[1], '\nTWIST:', best_params[2])
                 # print('Fraction of Water = ', fractional_volume, '<-- Example test case results' )
                 
-        degree_increment = (degree_increment * 11) / 16
-    # print('ROTATE:', best_params[0], '\nTILT:', best_params[1], '\nTWIST:', best_params[2])
-    # print('Fraction of Water = ', best_fractional_volume, '<-- Example test case results\n' )
+        degree_increment = (degree_increment * 13) / 16
+    print('ROTATE:', best_params[0], '\nTILT:', best_params[1], '\nTWIST:', best_params[2])
+    print('Fraction of Water = ', best_fractional_volume, '<-- Example test case results\n' )
     print('###########################################################################################')
     # BEST fraction 0.4999855276951166
