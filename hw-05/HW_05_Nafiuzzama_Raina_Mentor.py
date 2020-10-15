0c08b16328c12b0070eadb853623608f6ba8c268
Nikhil Raina <nxr5013@g.rit.edu>
2020-10-15 18:32:03 -0400

    Fixed the indentation for the file and removed trailing white spaces

--- hw-05/HW_05_Nafiuzzama_Raina_Mentor.py
+++ hw-05/HW_05_Nafiuzzama_Raina_Mentor.py
@@ -2,7 +2,7 @@
 Filename: HW05_Nafiuzzaman_Raina_Trained.py
 Name: Navid Nafiuzzaman <mxn4459@cs.rit.edu>
       Nikhil Raina       <nxr5013@cs.rit.edu>
-Description: This file builds a classifier by choosing the best attribute 
+Description: This file builds a classifier by choosing the best attribute
 based on the cost.
 """
 import pandas as pd
@@ -12,7 +12,7 @@ import textwrap
 """
 Build a pandas dataframe for the input CSV file
 
-Parameter: CSV input file     
+Parameter: CSV input file
 Return: Pandas DataFrame
 """
 def make_dataFrame(fileName):
@@ -20,13 +20,13 @@ def make_dataFrame(fileName):
     return dataFrame
 
 """
-Return the total number of values that are less than or equal to the current 
+Return the total number of values that are less than or equal to the current
 threshold in the current feature data being passed in
 
 feature_data: feature data being passed in
 current_threshold: the current threshold with regards to the feature data
 target: [Assam or Bhuttan]
-target_attr_category_data: The list of the Class column 
+target_attr_category_data: The list of the Class column
 """
 def less_than_sum_categories(feature_data, current_threshold, target, target_attr_category_data):
     total = 0
@@ -44,7 +44,7 @@ current feature data being passed in
 feature_data: feature data being passed in
 current_threshold: the current threshold with regards to the feature data
 target: [Assam or Bhuttan]
-target_attr_category_data: The list of the Class column 
+target_attr_category_data: The list of the Class column
 """
 def greater_than_sum_categories(feature_data, current_threshold, target, target_attr_category_data):
     total = 0
@@ -53,7 +53,7 @@ def greater_than_sum_categories(feature_data, current_threshold, target, target_
             if target_attr_category_data[index] == target:
                 total += 1
     return total
-    
+
 
 """
 Finds the best gini index and the threshold of the current data entered.
@@ -67,7 +67,7 @@ def best_threshold_gini_index(target_attr_category_data, feature_data, target_at
     max_value = max(feature_data)
 
     best_threshold = float("inf")
-    
+
     # This is the worst value for the Gini index
     gini_index = 0.5
 
@@ -103,12 +103,12 @@ def attr_sum(data, target_attr):
 
 
 """
-Splits the current dataset, from the left, with respect to the current best 
+Splits the current dataset, from the left, with respect to the current best
 feature till the threshold and returns the dataset
 
 data: The current node or the dataset
 curr_best_feature: the current best feature from the current dataset
-threshold: the threshold 
+threshold: the threshold
 """
 def data_parser_left(data, curr_best_feature, threshold):
     target_data = data[curr_best_feature]
@@ -122,12 +122,12 @@ def data_parser_left(data, curr_best_feature, threshold):
 
 
 """
-Splits the current dataset, from the left, with respect to the current best 
+Splits the current dataset, from the left, with respect to the current best
 feature till the threshold and returns the dataset
 
 data: The current node or the dataset
 curr_best_feature: the current best feature from the current dataset
-threshold: the threshold 
+threshold: the threshold
 """
 def data_parser_right(data, curr_best_feature, threshold):
     target_data = data[curr_best_feature]
@@ -144,7 +144,7 @@ def data_parser_right(data, curr_best_feature, threshold):
 Decision tree algorithm for classification
 
 data: The current node or the dataset
-tab_sequence: Number of tabs to be added to the file when writing the trainer 
+tab_sequence: Number of tabs to be added to the file when writing the trainer
 target_attr_categories: [Assam, Bhuttan]
 file_obj: file obj where the data will be written to
 depth: recursive depth
@@ -157,7 +157,7 @@ def decision_tree(data, tab_sequence, target_attr_categories, file_obj, depth):
     # -> size of the node [current dataset] < 10
     # -> node [current dataset] > 95% of a specific class
     # -> tree depth of 10
-    if len(data["class"]) < 10 or (attr_1/len(data["class"]) > 0.95 or attr_2/len(data["class"])) > 0.95 or depth == 4:
+    if len(data["class"]) < 10 or (attr_1/len(data["class"]) > 0.95 or attr_2/len(data["class"])) > 0.95 or depth == 5:
         for tab_count in range(tab_sequence):
             file_obj.write('\t')
         out = 1
@@ -165,7 +165,7 @@ def decision_tree(data, tab_sequence, target_attr_categories, file_obj, depth):
             out = -1
         file_obj.write('displayList.append(' + str(out) + ')\n')
         return
-    
+
     # default values set for the best attributes for the following:
     # -> feature index
     # -> threshold
@@ -182,14 +182,14 @@ def decision_tree(data, tab_sequence, target_attr_categories, file_obj, depth):
         # updates the best attributes after implementing the gini index, if needed.
         if gini_index < best_attr[2]:
             best_attr = [key_feature, threshold, gini_index]
-    
+
     for tab_count in range(tab_sequence):
         file_obj.write('\t')
 
     # displays the header of the csv file
     file_obj.write('if feature_rows["' + best_attr[0][0].upper() + best_attr[0][1:] + '"] <= ' + str(gini_index) + ':\n')
 
-    # splits the left data from the current data to act as a node 
+    # splits the left data from the current data to act as a node
     left_data = data_parser_left(data, best_attr[0], best_attr[1])
 
     # recursive call
@@ -200,18 +200,18 @@ def decision_tree(data, tab_sequence, target_attr_categories, file_obj, depth):
 
     file_obj.write('else:\n')
 
-    # splits the right data from the current data to act as a node 
+    # splits the right data from the current data to act as a node
     right_data = data_parser_right(data, best_attr[0], best_attr[1])
 
     # recursive call
     decision_tree(right_data, tab_sequence + 1, target_attr_categories, file_obj, depth + 1)
 
-        
+
 """
 Writes the initial lines of code to the training file
 """
 def program_writer():
-    f = open("HW_05_Nafiuzzaman_Raina_Trained.py", "wt") 
+    f = open("HW_05_Nafiuzzaman_Raina_Trained.py", "wt")
     f.write("import csv \n")
     f.write(textwrap.dedent('''\
     def csv_parser(attr_num, threshold):
@@ -219,9 +219,9 @@ def program_writer():
         fileReader = csv.reader(open(fileName))
         displayList = list()
         for feature_rows in fileReader:
-        \t'''))
+        '''))
     return f
-    
+
 
 """
 Calculate the accuracy of the selected attribute's prediction
@@ -237,15 +237,15 @@ def calculate_accuracy(dataFrame, attribute_name, threshold):
             our_res.append(1)
         else:
             our_res.append(-1)
-    
+
     counter  = 0
 
-    # compares the predicted result with the actual result. 
+    # compares the predicted result with the actual result.
     for idx in range(0, len(our_res)):
         if our_res[idx] == actual_res_lst[idx]:
             counter += 1
-    
-    
+
+
     calc = (counter/len(actual_res_lst)) * 100
     print("The best attribute is:", attribute_name)
     print("Accuracy of our selected attribute is ", calc)
@@ -274,19 +274,19 @@ def round_data(dataFrame):
         "reach"     :  [],
         "earLobes"  :  []
     }
-        
+
     for index in range(0, len(age_list)):
         book["age"].append(round(age_list[index]/2.0)*2.0)          # round age to nearest 2 years
         book["ht"].append(round(ht_list[index]/5)*5)                # round the height to nearest 5 cm
         book["tailLn"].append(round(tailln_list[index]))            # round the TailLn
         book["hairLn"].append(round(hairln_list[index]))            # round the HairLn
-        book["bangLn"].append(round(bangln_list[index]))            # round the BangLn 
+        book["bangLn"].append(round(bangln_list[index]))            # round the BangLn
         book["reach"].append(round(reach_list[index]))              # round the Reach
         book["earLobes"].append(round(earlobes_list[index]))        # round the EarLobes
-    
+
     return book
 
-        
+
 """
 The main function of the program, entry point
 """
@@ -294,9 +294,9 @@ def main():
     # fileName = input("Please type in the Input CSV File (including .csv): ")     # the training file's name
     fileName = "C:/Users/nikhi/OneDrive/RIT/7th Semester/Principles of Data Mining/Data-Mining/hw-05/Abominable_Data_HW05_v725.csv"
     dataFrame_data = make_dataFrame(fileName)                           # dataFrame made from the training file
-    tab_sequence = 2
+    tab_sequence = 3
     book = round_data(dataFrame_data)
-    book["class"] = dataFrame_data["Class"].tolist()                       # target attribute list 
+    book["class"] = dataFrame_data["Class"].tolist()                       # target attribute list
     target_attr_categories = dataFrame_data["Class"].unique().tolist()         # target categories
     file_obj = program_writer()
     decision_tree(book, tab_sequence, target_attr_categories, file_obj, 0)
