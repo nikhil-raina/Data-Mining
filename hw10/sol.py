2c5167fd69e237e7c9b739f4bf8c0fa07b67791b
Navid Nafiuzzaman <mxn4459@rit.edu>
2020-11-05 19:35:54 -0500

    basic

--- hw10/sol.py
+++ hw10/sol.py
@@ -1,5 +1,9 @@
+import pathlib
 import pandas as pd
 import numpy as np
+from numpy import linalg as LA      # for eighen stuffs
+import matplotlib.pyplot as plt
+
 
 def make_dataFrame(fileName):
     dataFrame = pd.read_csv(fileName)
@@ -12,5 +16,13 @@ def main():
     dataFrame = make_dataFrame(fileName)
     labels_drop = ["EarLobes", "Age"]
     dataFrame = dataFrame.drop(labels=labels_drop, axis=1)         # earlob and age dropped
+    np_array = dataFrame.to_numpy()
+    # find the eighen vector
+    # find the eigenvalues 
+    # QUESTION: What to do with Hair, Talk , BanG
+    #
+    
+
+
 
 main()
