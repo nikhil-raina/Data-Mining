215edcbc0a21669be1c96deff64c2667030a910c
Navid Nafiuzzaman <mxn4459@rit.edu>
2020-11-16 16:08:56 -0500

    Starter code

---
+++ project_gps/GPS_to_KML.py
@@ -0,0 +1,20 @@
+
+
+kml_header = " "    # read the text file having the header txt
+kml_footer = " "    
+
+
+# Takes input of a coordinate and dump it to the text file
+def print_coords(fileName, coord):
+    return True
+
+# reads the text file, outputs the kml file
+def reader(txt_file, kml_file):
+    return 0
+
+# Detects the anomalies # maybe helper funciton?  
+def anomaly_detector():
+    return 0
+
+
+        
