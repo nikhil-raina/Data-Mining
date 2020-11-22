5279dfda7b8380820268e5d226488a02ffa1acee
Navid Nafiuzzaman <mxn4459@rit.edu>
2020-11-22 13:00:15 -0500

    Added todo list

--- project_gps/GPS_to_KML.py
+++ project_gps/GPS_to_KML.py
@@ -3,6 +3,12 @@ import sys
 kml_header = " "    # read the text file having the header txt
 kml_footer = " "    
 
+# TODO: Handle Parked Car
+# TODO: Driving in Straight line, ignore some points
+# TODO: If there is similar GPS Coords, ignore 
+# TODO: If there is missing Points, cover that
+# TODO: Handle case when the Car not Moving
+# TODO: Find speed of the car
 
 # Takes input of a coordinate and dump it to the text file
 def print_coords(fileName, coord):
@@ -23,21 +29,27 @@ def file_writer(file_name, str):
     file.close()
 
     
-
-def make_kml_header(kml_file):
-    fileObj = open("helper/kml_header.txt", "r")
+# header and footer of kml of the KML File
+def kml_static_text(kml_file, static_text):
+    fileObj = open(static_text, "r")
     data = fileObj.read()
     file_writer(kml_file, data)
 
-def main():
+    
 
+# entry point of the program
+def main():
     if len(sys.argv) !=3:
         print("Usage: python3 GPS_to_KML.py GPS_FileName.txt KML_Filename.txt")
         exit()
 
     gps_file, kml_file = sys.argv[1], sys.argv[2]           # get the 2 file names
 
-    make_kml_header(kml_file)
+    kml_static_text(kml_file, "helper/kml_header.txt")      # generates the header of the kml
+
+
+    # kml_static_text (kml_file, "helper/kml_footer.txt")     # generates the footer of the kml 
+
 
 
 main()
