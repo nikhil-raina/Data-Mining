d63d87f1ec86bcc329f91eaab67015523a6852be
Nikhil Raina <nxr5013@g.rit.edu>
2020-11-30 18:19:06 -0500

    Updated the input format and updated the writers for the data for user inputs

--- project_gps/GPS_to_CostMap.py
+++ project_gps/GPS_to_CostMap.py
@@ -6,6 +6,7 @@ Description: Converts NMEA GPS TXT files to Cost map KML files
 """
 import sys
 import pandas as pd
+from os import mkdir
 from preprocess_gps_data import preprocessor, find_stops, find_all_turns
 import glob as g
 
@@ -13,9 +14,7 @@ import glob as g
 # Write the path of the KML files
 
 
-def write_to_path_kml(gps_file, processed_data_df):
-    output_file = '/path_kml_files/' + \
-        gps_file.split('\\')[1].split('.')[0] + '.kml'
+def write_to_path_kml(output_file, processed_data_df):
     with open(output_file, 'w') as out:
         header = open('/helper/kml_header.txt', 'r')
         out.write(header.read()+'\n')
@@ -47,23 +46,25 @@ def write_to_stop_kml(out, stops):
 # Writes the Left Right KML Files
 
 
-def write_to_left_right_kml(gps_file, left_turns, right_turns):
-    output_file = '/left_right_kml_files/' + \
-        gps_file.split('\\')[1].split('.')[0] + '.kml'
+def write_to_left_right_kml(output_file, left_turns, right_turns):
     with open(output_file, 'w') as out:
         header = open('/helper/kml_pins_header.txt', 'r')
         out.write(header.read() + '\n')
+
+        # adding all the left turns to the kml file
         for i in range(0, len(left_turns)):
             read_and_write(out, '/helper/left_header.txt')
             out.write(left_turns[i][0] + ',' + left_turns[i][1] + '\n')
             read_and_write(out, '/helper/placemark_footer.txt')
             out.write('\n')
 
+        # adding all the right turns to the kml file
         for i in range(0, len(right_turns)):
             read_and_write(out, '/helper/right_header.txt')
             out.write(right_turns[i][0] + ',' + right_turns[i][1] + '\n')
             read_and_write(out, '/helper/placemark_footer.txt')
             out.write('\n')
+
         footer = open('/helper/kml_pins_footer.txt', 'r')
         out.write(footer.read() + '\n')
         footer.close()
@@ -96,8 +97,10 @@ def read_all_GPS_data():
     # list_of_files = g.glob('/gps_files/*.txt')
     list_of_files = g.glob('/gps_files/*_gps_file.txt')
     for gps_file in list_of_files:
-        output_file = '/path_kml_files/' + \
-            gps_file.split('\\')[1].split('.')[0] + '.kml'
+        output_file_path = '/path_kml_files/' + gps_file.split('\\')[1].split('.')[0] + '.kml'
+        output_file_left_right = '/left_right_kml_files/' + gps_file.split('\\')[1].split('.')[0] + '.kml'
+        output_file_stop = '/stop_kml_files/' + gps_file.split('\\')[1].split('.')[0] + '.kml'
+
         gps_data = read_gps_data(gps_file)
         processed_data_df = preprocessor(gps_data)
         all_stops_data = find_stops(processed_data_df.values.tolist())
@@ -105,13 +108,17 @@ def read_all_GPS_data():
             processed_data_df.values.tolist())
 
         # writes the kml data on to the corresponding files
-        write_to_path_kml(gps_file, processed_data_df.values.tolist())
-        write_to_left_right_kml(gps_file, left_turns, right_turns)
-        with open('/stop_kml_files/' + gps_file.split('\\')[1].split('.')[0] + '.kml', 'w') as out:
+        write_to_path_kml(output_file_path, processed_data_df.values.tolist())
+        write_to_left_right_kml(output_file_left_right, left_turns, right_turns)
+        with open(output_file_stop, 'w') as out:
             write_to_stop_kml(out, all_stops_data)
 
 
-def read_input_data(gps_file, kml_file):
+def read_input_data(gps_file, kml_directory):
+    try:
+        mkdir(kml_directory)
+    except OSError as e:
+        print(e)
     gps_data = read_gps_data(gps_file)
     processed_data_df = preprocessor(gps_data)
     all_stops_data = find_stops(processed_data_df.values.tolist())
@@ -119,16 +126,17 @@ def read_input_data(gps_file, kml_file):
         processed_data_df.values.tolist())
 
     # writes the kml data on to the corresponding files
-    write_to_path_kml(gps_file, processed_data_df.values.tolist())
-    write_to_left_right_kml(gps_file, left_turns, right_turns)
-    write_to_stop_kml(gps_file, all_stops_data)
+    write_to_path_kml(kml_directory + '/kml_path.kml', processed_data_df.values.tolist())
+    write_to_left_right_kml(kml_directory + '/kml_left_right.kml', left_turns, right_turns)
+    with open(kml_directory + '/kml_stops.kml', 'w') as out:
+        write_to_stop_kml(out, all_stops_data)
 
 
 # entry point of the program
 def main():
     if len(sys.argv) != 3:
         read_all_GPS_data()
-        print("Usage: python3 GPS_to_KML.py GPS_FileName.txt KML_Filename.txt")
+        print("Usage: python3 GPS_to_KML.py GPS_FileName.txt KML_Directory")
     else:
         # a = '/kml/ZJ42_EC0_to_RIT.TXT'
         # b = '/test_examples/test2.kml'
