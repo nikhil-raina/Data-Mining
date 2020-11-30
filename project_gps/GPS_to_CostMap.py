acbd3ba2c4fdb0bc0cc7206558b2fef7d03470ba
Navid Nafiuzzaman <mxn4459@rit.edu>
2020-11-29 23:54:28 -0500

    Final Submission before 12 AM

--- project_gps/GPS_to_CostMap.py
+++ project_gps/GPS_to_CostMap.py
@@ -1,117 +1,140 @@
+"""
+Name:   Navid Nafiuzzaman
+        Nikhil Raina
+Filename: GPS_to_CostMap.py
+Description: Converts NMEA GPS TXT files to Cost map KML files
+"""
 import sys
-import pynmea2 as nmea
 import pandas as pd
+from preprocess_gps_data import preprocessor, find_stops, find_all_turns
+import glob as g
 
-kml_header = " "    # read the text file having the header txt
-kml_footer = " "    
-bigDataframe  = pd.DataFrame(columns=["IDen", "Time", "Latitude", "Longitutde", "Altitude"])
 
-# TODO: Handle Parked Car
-# TODO: Driving in Straight line, ignore some points
-# TODO: If there is similar GPS Coords, ignore 
-# TODO: If there is missing Points, cover that
-# TODO: Handle case when the Car not Moving
-# TODO: Find speed of the car
+# Write the path of the KML files
 
-# this function appends string to specified file
-def file_writer(file_name, str):
-    file = open(file_name, "a")
-    file.write(str)
+
+def write_to_path_kml(gps_file, processed_data_df):
+    output_file = '/path_kml_files/' + \
+        gps_file.split('\\')[1].split('.')[0] + '.kml'
+    with open(output_file, 'w') as out:
+        header = open('/helper/kml_header.txt', 'r')
+        out.write(header.read()+'\n')
+        header.close()
+        for data_set in processed_data_df:
+            out.write(','.join(data_set[0:3]))
+            out.write('\n')
+        footer = open('/helper/kml_footer.txt', 'r')
+        out.write(footer.read() + '\n')
+        header.close()
+        footer.close()
+    print('Added to ===>', output_file)
+    return
+
+# Writes the Stop KML files
+
+
+def write_to_stop_kml(out, stops):
+    read_and_write(out, '/helper/kml_pins_header.txt')
+    for stop in stops:
+        read_and_write(out, '/helper/stop_header.txt')
+        out.write(stop[0] + ',' + stop[1] + '\n')
+        read_and_write(out, '/helper/placemark_footer.txt')
+        out.write('\n')
+    read_and_write(out, '/helper/kml_pins_footer.txt')
+    print('Added to ===>', out)
+    return
+
+# Writes the Left Right KML Files
+
+
+def write_to_left_right_kml(gps_file, left_turns, right_turns):
+    output_file = '/left_right_kml_files/' + \
+        gps_file.split('\\')[1].split('.')[0] + '.kml'
+    with open(output_file, 'w') as out:
+        header = open('/helper/kml_pins_header.txt', 'r')
+        out.write(header.read() + '\n')
+        for i in range(0, len(left_turns)):
+            read_and_write(out, '/helper/left_header.txt')
+            out.write(left_turns[i][0] + ',' + left_turns[i][1] + '\n')
+            read_and_write(out, '/helper/placemark_footer.txt')
+            out.write('\n')
+
+        for i in range(0, len(right_turns)):
+            read_and_write(out, '/helper/right_header.txt')
+            out.write(right_turns[i][0] + ',' + right_turns[i][1] + '\n')
+            read_and_write(out, '/helper/placemark_footer.txt')
+            out.write('\n')
+        footer = open('/helper/kml_pins_footer.txt', 'r')
+        out.write(footer.read() + '\n')
+        footer.close()
+        header.close()
+    print('Added to ===>', output_file)
+    return
+
+
+# Rights the file
+def read_and_write(output_file, file):
+    output_file.write(open(file).read())
+    output_file.write("\n")
+
+
+"""
+reads the data from the GPS file and returns a list.
+"""
+
+
+def read_gps_data(file_name):
+    file = open(file_name, 'r')
+
+    # removing the header and reading the lines
+    lines = file.readlines()[5:]
     file.close()
+    return lines
 
 
-def gpgga(msg, new_kml):
-    latitude = round(msg.latitude, 6)           # latitude in 6 decimal
-    latitude = format(latitude, ".6f")          # to add padding 0
+def read_all_GPS_data():
+    # list_of_files = g.glob('/gps_files/*.txt')
+    list_of_files = g.glob('/gps_files/*_gps_file.txt')
+    for gps_file in list_of_files:
+        output_file = '/path_kml_files/' + \
+            gps_file.split('\\')[1].split('.')[0] + '.kml'
+        gps_data = read_gps_data(gps_file)
+        processed_data_df = preprocessor(gps_data)
+        all_stops_data = find_stops(processed_data_df.values.tolist())
+        left_turns, right_turns, delta_percent = find_all_turns(
+            processed_data_df.values.tolist())
 
-    longitude = round(msg.longitude, 6)         # longitude in 6 decimal
-    longitude = format(longitude, ".6f")        # to add padding 0
+        # writes the kml data on to the corresponding files
+        write_to_path_kml(gps_file, processed_data_df.values.tolist())
+        write_to_left_right_kml(gps_file, left_turns, right_turns)
+        with open('/stop_kml_files/' + gps_file.split('\\')[1].split('.')[0] + '.kml', 'w') as out:
+            write_to_stop_kml(out, all_stops_data)
 
-    alt = (msg.altitude)                        # altitude
-    curTime = msg.timestamp
 
-    output = str(longitude) + "," + str(latitude) + "," + str(alt) + "\n"
-    
-    rowToAppend = ["$GPGGA", curTime, latitude, longitude, alt] 
-    
-    cur_df_size = len(bigDataframe)
-    bigDataframe.loc[cur_df_size] = rowToAppend
-    file_writer(new_kml, output)
+def read_input_data(gps_file, kml_file):
+    gps_data = read_gps_data(gps_file)
+    processed_data_df = preprocessor(gps_data)
+    all_stops_data = find_stops(processed_data_df.values.tolist())
+    left_turns, right_turns, delta_percent = find_all_turns(
+        processed_data_df.values.tolist())
 
-def grmc(msg):
-    latitude = round(msg.latitude, 6)           # latitude in 6 decimal
-    latitude = format(latitude, ".6f")          # to add padding 0
+    # writes the kml data on to the corresponding files
+    write_to_path_kml(gps_file, processed_data_df.values.tolist())
+    write_to_left_right_kml(gps_file, left_turns, right_turns)
+    write_to_stop_kml(gps_file, all_stops_data)
 
-    longitude = round(msg.longitude, 6)         # longitude in 6 decimal
-    longitude = format(longitude, ".6f")        # to add padding 0
-    curTime = msg.timestamp
-
-    rowToAppend = ["$GPRMC", curTime, latitude, longitude, 0]
-     
-    cur_df_size = len(bigDataframe)
-    bigDataframe.loc[cur_df_size] = rowToAppend
-    
-    
-# header and footer of kml of the KML File
-def kml_static_text(kml_file, header_txt, footer_txt, new_kml):
-    header = open(header_txt, "r").read()   # reads the header of kml file
-    file_writer(new_kml, header)            # writes the header of kml file
-    with open(kml_file) as file:  
-        for _ in range(5):      # skips the first few 5 lines
-            next(file)          
-        for line in file:
-            try:
-                msg = nmea.parse(line)
-                splitted = line.split(",")
-                if splitted[0] == "$GPGGA":
-                    print("GGA Sentence")
-                    gpgga(msg, new_kml)
-                else:
-                    print("RMC sentence")
-                    grmc(msg)
-            except nmea.ParseError as e:
-                print('Parse error: {}'.format(e))
-                continue 
-    
-
-    footer = open(footer_txt, "r").read()
-    file_writer(new_kml, footer)
-    print("Successfully Completed")
-              
 
 # entry point of the program
 def main():
-    if len(sys.argv) !=3:
+    if len(sys.argv) != 3:
+        read_all_GPS_data()
         print("Usage: python3 GPS_to_KML.py GPS_FileName.txt KML_Filename.txt")
-        exit()
-
-    gps_file, kml_file = sys.argv[1], sys.argv[2]           # get the 2 file names
-    
-    kml_static_text(gps_file, "helper/kml_header.txt", "helper/kml_footer.txt", kml_file)      # generates the header of the kml
-
-
-    # kml_static_text (kml_file, "helper/kml_footer.txt")     # generates the footer of the kml 
+    else:
+        # a = '/kml/ZJ42_EC0_to_RIT.TXT'
+        # b = '/test_examples/test2.kml'
+        read_input_data(sys.argv[1], sys.argv[2])
+    # read_input_data(a, b)
 
 
-
-main()
-
-
-
-
-
-
-
-##### Parsing
-
-# Takes input of a coordinate and dump it to the text file
-def print_coords(fileName, coord):
-    return True
-
-# reads the text file, outputs the kml file
-def reader(txt_file, kml_file):
-    return 0
-
-# Detects the anomalies # maybe helper funciton?  
-def anomaly_detector():
-    return 0
+if __name__ == '__main__':
+    main()
--- project_gps/GPS_to_KML.py
+++ project_gps/GPS_to_KML.py
@@ -1,135 +1,96 @@
+"""
+Name:   Navid Nafiuzzaman
+        Nikhil Raina
+Filename: GPS_to_KML.py
+Description: Converts NMEA GPS TXT files to KML files.
+"""
 import sys
+import pynmea2 as nmea
 import pandas as pd
-from preprocess_gps_data import preprocessor, find_stops, find_all_turns
-import glob as g
+
+kml_header = " "    # read the text file having the header txt
+kml_footer = " "
+bigDataframe = pd.DataFrame(
+    columns=["IDen", "Time", "Latitude", "Longitutde", "Altitude"])
 
 
-# TODO(done): Handle Parked Car
-# TODO(done): Driving in Straight line, ignore some points
-# TODO(done: not ignoring): If there is similar GPS Coords, ignore 
-# TODO(done: there arent missing points so didnt have such an issue): If there is missing Points, cover that
-# TODO(done: same as car being 'parked'): Handle case when the Car not Moving
-# TODO(done): Find speed of the car
-
-
-def write_to_path_kml(gps_file, processed_data_df):
-    output_file = 'project_gps/path_kml_files/' + gps_file.split('\\')[1].split('.')[0] + '.kml'
-    with open(output_file, 'w') as out:
-        header = open('project_gps/helper/kml_header.txt','r')
-        out.write(header.read()+'\n')
-        header.close()
-        for data_set in processed_data_df:
-            out.write(','.join(data_set[0:3]))
-            out.write('\n')
-        footer = open('project_gps/helper/kml_footer.txt', 'r')
-        out.write(footer.read() + '\n')
-        header.close()
-        footer.close()
-    print('Added to ===>', output_file)
-    return
-
-
-
-def write_to_stop_kml(out, stops):
-    read_and_write(out, 'project_gps/helper/kml_pins_header.txt')
-    for stop in stops:
-        read_and_write(out, 'project_gps/helper/stop_header.txt')
-        out.write(stop[0] + ',' + stop[1] + '\n')
-        read_and_write(out, 'project_gps/helper/placemark_footer.txt')
-        out.write('\n')
-    read_and_write(out, 'project_gps/helper/kml_pins_footer.txt')
-    print('Added to ===>', out)
-    return    
-
-
-
-def write_to_left_right_kml(gps_file, left_turns, right_turns):
-    output_file = 'project_gps/left_right_kml_files/' + gps_file.split('\\')[1].split('.')[0] + '.kml'
-    with open(output_file, 'w') as out:
-        header = open('project_gps/helper/kml_pins_header.txt', 'r')
-        out.write(header.read() + '\n')
-        for i in range(0, len(left_turns)):
-            read_and_write(out, 'project_gps/helper/left_header.txt')
-            out.write(left_turns[i][0] + ',' + left_turns[i][1] + '\n')
-            read_and_write(out, 'project_gps/helper/placemark_footer.txt')
-            out.write('\n')
-
-        for i in range(0, len(right_turns)):
-            read_and_write(out, 'project_gps/helper/right_header.txt')
-            out.write(right_turns[i][0] + ',' + right_turns[i][1] + '\n')
-            read_and_write(out, 'project_gps/helper/placemark_footer.txt')
-            out.write('\n')
-        footer = open('project_gps/helper/kml_pins_footer.txt', 'r')
-        out.write(footer.read() + '\n')
-        footer.close()
-        header.close()
-    print('Added to ===>', output_file)
-    return
-
-
-
-def read_and_write(output_file, file):
-    output_file.write(open(file).read())
-    output_file.write("\n")
-
-
-
-"""
-reads the data from the GPS file and returns a list.
-"""
-def read_gps_data(file_name):
-    file = open(file_name, 'r')
-
-    # removing the header and reading the lines
-    lines = file.readlines()[5:]
+def file_writer(file_name, str):
+    file = open(file_name, "a")
+    file.write(str)
     file.close()
-    return lines
 
 
+def gpgga(msg, new_kml):
+    latitude = round(msg.latitude, 6)           # latitude in 6 decimal
+    latitude = format(latitude, ".6f")          # to add padding 0
 
-def read_all_GPS_data():
-    # I am not sure what all things need to parse here
-    # list_of_files = g.glob('project_gps/gps_files/*.txt')
-    list_of_files = g.glob('project_gps/gps_files/*_gps_file.txt')
-    for gps_file in list_of_files:
-        output_file = 'project_gps/path_kml_files/' + gps_file.split('\\')[1].split('.')[0] + '.kml'
-        gps_data = read_gps_data(gps_file)
-        processed_data_df = preprocessor(gps_data)
-        all_stops_data = find_stops(processed_data_df.values.tolist())
-        left_turns, right_turns, delta_percent = find_all_turns(processed_data_df.values.tolist())
+    longitude = round(msg.longitude, 6)         # longitude in 6 decimal
+    longitude = format(longitude, ".6f")        # to add padding 0
 
-        # writes the kml data on to the corresponding files
-        write_to_path_kml(gps_file, processed_data_df.values.tolist())
-        write_to_left_right_kml(gps_file, left_turns, right_turns)
-        with open('project_gps/stop_kml_files/' + gps_file.split('\\')[1].split('.')[0] + '.kml', 'w') as out:
-            write_to_stop_kml(out, all_stops_data)
+    alt = (msg.altitude)                        # altitude
+    curTime = msg.timestamp
+
+    output = str(longitude) + "," + str(latitude) + "," + str(alt) + "\n"
+
+    rowToAppend = ["$GPGGA", curTime, latitude, longitude, alt]
+
+    cur_df_size = len(bigDataframe)
+    bigDataframe.loc[cur_df_size] = rowToAppend
+    file_writer(new_kml, output)
 
 
+def grmc(msg):
+    latitude = round(msg.latitude, 6)           # latitude in 6 decimal
+    latitude = format(latitude, ".6f")          # to add padding 0
 
-def read_input_data(gps_file, kml_file):
-    gps_data = read_gps_data(gps_file)
-    processed_data_df = preprocessor(gps_data)
-    all_stops_data = find_stops(processed_data_df.values.tolist())
-    left_turns, right_turns, delta_percent = find_all_turns(processed_data_df.values.tolist())
-    
-    # writes the kml data on to the corresponding files
-    write_to_path_kml(gps_file, processed_data_df.values.tolist())
-    write_to_left_right_kml(gps_file, left_turns, right_turns)
-    write_to_stop_kml(gps_file, all_stops_data)
+    longitude = round(msg.longitude, 6)         # longitude in 6 decimal
+    longitude = format(longitude, ".6f")        # to add padding 0
+    curTime = msg.timestamp
+
+    rowToAppend = ["$GPRMC", curTime, latitude, longitude, 0]
+
+    cur_df_size = len(bigDataframe)
+    bigDataframe.loc[cur_df_size] = rowToAppend
+
+
+# header and footer of kml of the KML File
+def kml_static_text(kml_file, header_txt, footer_txt, new_kml):
+    header = open(header_txt, "r").read()   # reads the header of kml file
+    file_writer(new_kml, header)            # writes the header of kml file
+    with open(kml_file) as file:
+        for _ in range(5):      # skips the first few 5 lines
+            next(file)
+        for line in file:
+            try:
+                msg = nmea.parse(line)
+                splitted = line.split(",")
+                if splitted[0] == "$GPGGA":
+                    print("GGA Sentence")
+                    gpgga(msg, new_kml)
+                else:
+                    print("RMC sentence")
+                    grmc(msg)
+            except nmea.ParseError as e:
+                print('Parse error: {}'.format(e))
+                continue
+
+    footer = open(footer_txt, "r").read()
+    file_writer(new_kml, footer)
+    print("Successfully Completed")
 
 
 # entry point of the program
 def main():
     if len(sys.argv) != 3:
-        read_all_GPS_data()
         print("Usage: python3 GPS_to_KML.py GPS_FileName.txt KML_Filename.txt")
-    else:
-    # a = 'project_gps/kml/ZJ42_EC0_to_RIT.TXT'
-    # b = 'project_gps/test_examples/test2.kml'
-        read_input_data(sys.argv[1], sys.argv[2])
-    # read_input_data(a, b)
+        exit()
+
+    # get the 2 file names
+    gps_file, kml_file = sys.argv[1], sys.argv[2]
+
+    # generates the header of the kml
+    kml_static_text(gps_file, "helper/kml_header.txt",
+                    "helper/kml_footer.txt", kml_file)      # processes the TXT file
 
 
-
-if __name__ == '__main__':
-    main()
+main()
